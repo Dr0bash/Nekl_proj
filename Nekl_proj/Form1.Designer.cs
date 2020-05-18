3139a4cd5bf493c75438cb3f1507f97ffc1f6051
Dr0bash <igor2-shamaev@yandex.ru>
2020-05-18 21:30:39 +0300

    added first things

--- Nekl_proj/Form1.Designer.cs
+++ Nekl_proj/Form1.Designer.cs
@@ -29,12 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(415, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1355, 642);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Nekl_proj/Form1.cs
+++ Nekl_proj/Form1.cs
@@ -7,14 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
 
 namespace Nekl_proj
 {
     public partial class Form1 : Form
     {
+        static double scale = 1.0;
+        static PictureBox LeftBox = null;
+        static PictureBox TopBox = null;
+        static PictureBox WaterLeft = null;
+        static TextBox tbox = null;
+        static Size SettingsSize = new Size();
+        static System.Timers.Timer Timer;
+        static double WaterLevel = 100.0;
+        static double time = 0;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            SettingsSize.Width = 200;
+            SettingsSize.Height = Size.Height;
+            TopBox = new PictureBox
+            {
+                Location = new Point(5, 5),
+                Size = new Size(Size.Width - SettingsSize.Width - 5, Size.Height/2 - 5),
+                BackColor = Color.Cyan,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            LeftBox = new PictureBox
+            {
+                Location = new Point(5, Size.Height / 2 + 5),
+                Size = new Size(Size.Width - SettingsSize.Width - 5, Size.Height/2 - 50),
+                BackColor = Color.White,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            WaterLeft = new PictureBox
+            {
+                Location = new Point(5, LeftBox.Location.Y + LeftBox.Size.Height - (int)WaterLevel),
+                Size = new Size(LeftBox.Size.Width, (int)WaterLevel),
+                BackColor = Color.Cyan,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+
+            Controls.Add(WaterLeft);
+            Controls.Add(LeftBox);
+            Controls.Add(TopBox);
+            
+            timer1.Start();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            double CurrentLevel = 40;
+            WaterLeft.Location = new Point(5, LeftBox.Location.Y + LeftBox.Size.Height - (int)(WaterLevel + CurrentLevel* Math.Sin(time)));
+            WaterLeft.Size = new Size(LeftBox.Size.Width, (int)(WaterLevel + CurrentLevel* Math.Sin(time)));
+            time += 0.05;
         }
     }
 }
