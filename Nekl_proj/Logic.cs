543b82d276bd2c2cfbc3928c7e26b15efb1739ed
Oxana <lereenadem@yandex.ru>
2020-05-31 02:50:07 +0300

    Add logic

--- Nekl_proj/Logic.cs
+++ Nekl_proj/Logic.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Nekl_proj
 {
@@ -7,54 +8,119 @@ namespace Nekl_proj
     // private var logic = new Logic(
     //     WorldCharacteristics.MaxHorizontalCraneSpeed * WorldCharacteristics.TimeDimension,
     //     WorldCharacteristics.MaxRopeDownSpeed * WorldCharacteristics.TimeDimension);
-    
+
     // Наружу торчат методы которые говорят должное перемещение по вертикали и горизонтали
 
     public class Logic
     {
         private readonly double _maxDeviationSpeedPerTick;
         private readonly double _maxHeightSpeedPerTick;
-        
-        private readonly LogicGraph _deviationGraph = new LogicGraph(3); // тут наверное не 3 будет
+
+        private readonly LogicGraph _deviationGraph = new LogicGraph(2);
         private readonly LogicGraph _heightGraph = new LogicGraph(3);
         private readonly LogicGraph _speedGraph = new LogicGraph(3);
-        
-        private enum DeviationType { /* TODO: пока непонятно */ }
-        private enum HeightType { VeryClose, Close, Far }
-        private enum SpeedType { Up, DownSlow, DownFast }
+
+        private enum DeviationType
+        {
+            Controlled,
+            Uncontrolled
+        }
+
+        private enum HeightType
+        {
+            VeryClose,
+            Close,
+            Far
+        }
+
+        private enum SpeedType
+        {
+            Up,
+            DownSlow,
+            DownFast
+        }
 
         public Logic(double maxDeviationSpeedPerTick, double maxHeightSpeedPerTick)
         {
             _maxDeviationSpeedPerTick = maxDeviationSpeedPerTick;
             _maxHeightSpeedPerTick = maxHeightSpeedPerTick;
 
-            // правила для height
+            _deviationGraph.AddLogicTrapeze((int) DeviationType.Controlled, new LogicTrapeze(0, 0, 3, 5));
+            _deviationGraph.AddLogicTrapeze((int) DeviationType.Uncontrolled, new LogicTrapeze(3, 5, 100, 100));
+
             _heightGraph.AddLogicTrapeze((int) HeightType.VeryClose, new LogicTrapeze(-100, -100, 2, 3));
             _heightGraph.AddLogicTrapeze((int) HeightType.Close, new LogicTrapeze(2, 3, 5, 6));
             _heightGraph.AddLogicTrapeze((int) HeightType.Far, new LogicTrapeze(5, 6, 100, 100));
-        
-            // правила для speed
+
             _speedGraph.AddLogicTrapeze((int) SpeedType.Up, new LogicTrapeze(-1, -1, -1, 0));
             _speedGraph.AddLogicTrapeze((int) SpeedType.DownSlow, new LogicTrapeze(-1, 0, 1, 2));
             _speedGraph.AddLogicTrapeze((int) SpeedType.DownFast, new LogicTrapeze(1, 2, 2, 2));
         }
-        
+
         public double DeviationCompensation(double horizontalMove)
-        { 
+        {
             if (Math.Abs(horizontalMove) < _maxDeviationSpeedPerTick)
                 return -horizontalMove;
-        
+
             return horizontalMove < 0 ? _maxDeviationSpeedPerTick : -_maxDeviationSpeedPerTick;
         }
-        
+
+        private int[,] rules =
+        {
+            {0, 1, 2},
+            {0, 0, 1}
+        };
+
         public double HeightCompensation(double horizontalMove, double distance)
         {
-            return 0;
+            if (distance <= _maxHeightSpeedPerTick)
+                return distance;
+
+            var deviationDistribution = _deviationGraph.GetDistribution(Math.Abs(horizontalMove));
+            var heightDistribution = _heightGraph.GetDistribution(distance);
+
+            var speedDistribution = new List<double>(3);
+            for (var i = 0; i < deviationDistribution.Count; ++i)
+            for (var j = 0; j < heightDistribution.Count; ++j)
+                speedDistribution[rules[i, j]] =
+                    Math.Max(speedDistribution[rules[i, j]], Math.Min(deviationDistribution[i], heightDistribution[j]));
+
+            var polygon = _speedGraph.GetPolygon(speedDistribution);
+            var centerOfMass = CenterOfMass(polygon);
+
+            return centerOfMass.X;
         }
-        
+
         private Point2D CenterOfMass(List<Point2D> points)
         {
-            return null;
+            var point = new Point2D(0, 0);
+            double x = 0, y = 0, s = 0;
+
+            for (var i = 0; i < points.Count - 1; i++)
+            {
+                var signArea = SignArea(point, points[i], points[i + 1]);
+                var centroid = Centroid(point, points[i], points[i + 1]);
+                x += centroid.X * signArea;
+                y += centroid.Y * signArea;
+                s += signArea;
+            }
+
+            if (s != 0)
+                return new Point2D(x / s, y / s);
+
+            // прямая => среднее по Х
+            x = points.Sum(t => t.X);
+            return new Point2D(x / points.Count, 0);
+        }
+
+        private double SignArea(Point2D p1, Point2D p2, Point2D p3)
+        {
+            return ((p2.X - p1.X) * (p3.Y - p1.Y) - (p2.Y - p1.Y) * (p3.X - p1.X)) / 2;
+        }
+
+        private Point2D Centroid(Point2D p1, Point2D p2, Point2D p3)
+        {
+            return new Point2D((p1.X + p2.X + p3.X) / 3, (p1.Y + p2.Y + p3.Y) / 3);
         }
     }
 }
--- Nekl_proj/LogicGraph.cs
+++ Nekl_proj/LogicGraph.cs
@@ -19,7 +19,30 @@ namespace Nekl_proj
         public List<double> GetDistribution(double x)
         {
             var res = new List<double>(new double[_list.Count]);
-            // TODO 
+            for (var i = 0; i < _list.Count; ++i)
+            {
+                var c = _list[i];
+                if (x < c.BottomLeft || c.BottomRight < x)
+                {
+                    res[i] = 0;
+                    continue;
+                }
+
+                if (c.TopLeft <= x && x <= c.TopRight)
+                {
+                    res[i] = 1;
+                    continue;
+                }
+
+                if (c.BottomLeft <= x && x < c.TopLeft)
+                {
+                    res[i] = (x - c.BottomLeft) / (c.TopLeft - c.BottomLeft);
+                    continue;
+                }
+
+                res[i] = 1 - (x - c.TopRight) / (c.BottomRight - c.TopRight);
+            }
+
             return res;
         }
 
@@ -28,8 +51,66 @@ namespace Nekl_proj
             var res = new List<Point2D>();
             if (distribution.Count != _list.Count)
                 return null;
-            // TODO
-            
+
+            var j = 0;
+            while (distribution[j] == 0)
+                j++;
+
+            var t1 = _list[j].Cut(distribution[j]);
+            res.Add(t1.BottomLeft);
+            res.Add(t1.TopLeft);
+
+            for (var i = j + 1; i < _list.Count; i++)
+            {
+                if (distribution[i] == 0)
+                    continue;
+
+                var t2 = _list[i].Cut(distribution[i]);
+                var intersection = Point2D.LinesIntersection(t1.TopRight, t1.BottomRight, t2.BottomLeft, t2.TopLeft);
+                if (intersection != null)
+                {
+                    if (t1.TopRight.Y >= intersection.Y)
+                    {
+                        res.Add(t1.TopRight);
+                        if (t2.TopLeft.Y > intersection.Y)
+                        {
+                            res.Add(t2.TopLeft);
+                            res.Add(intersection);
+                        }
+                        else
+                        {
+                            res.Add(Point2D.LineXForY(t1.TopRight, t1.BottomRight, distribution[i]));
+                        }
+                    }
+                    else
+                    {
+                        if (t2.TopLeft.Y > intersection.Y)
+                        {
+                            res.Add(Point2D.LineXForY(t2.BottomLeft, t2.TopLeft, distribution[i - 1]));
+                            res.Add(t2.TopLeft);
+                        }
+                        else
+                        {
+                            if (t1.TopRight.Y > t2.TopLeft.Y)
+                            {
+                                res.Add(t1.TopRight);
+                                res.Add(Point2D.LineXForY(t1.TopRight, t1.BottomRight, distribution[i]));
+                            }
+                            else if (t1.TopRight.Y < t2.TopLeft.Y)
+                            {
+                                res.Add(t2.TopLeft);
+                                res.Add(Point2D.LineXForY(t2.BottomLeft, t2.TopLeft, distribution[i - 1]));
+                            }
+                        }
+                    }
+                }
+
+                t1 = t2;
+            }
+
+            res.Add(t1.TopRight);
+            res.Add(t1.BottomRight);
+
             return res;
         }
     }
